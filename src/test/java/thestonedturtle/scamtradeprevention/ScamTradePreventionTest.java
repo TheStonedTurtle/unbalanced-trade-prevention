082a6498798526fe2c2d44b0e1ee3bf99a43b676
TheStonedTurtle <mgoodwin559@gmail.com>
2024-03-23 13:33:27 -0700

    Rename all files

--- src/test/java/thestonedturtle/scamtradeprevention/ScamTradePreventionTest.java
+++ src/test/java/thestonedturtle/scamtradeprevention/ScamTradePreventionTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package thestonedturtle.scamtradeprevention;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ScamTradePreventionTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ScamTradePreventionPlugin.class);
 		RuneLite.main(args);
 	}
 }
